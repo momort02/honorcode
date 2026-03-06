49e1bfbdd63441839a1bb5b60ad69fb5068810fc
momort02 <amaury.goemaere@gmail.com>
2026-03-06 07:45:06 +0000

    mieux

---
+++ config.js
@@ -0,0 +1,5 @@
+// Ce fichier est généré automatiquement par GitHub Actions
+// NE PAS modifier manuellement — les valeurs seront écrasées au prochain déploiement
+// NE PAS committer de vraies valeurs ici
+window.AIRTABLE_TOKEN   = '';
+window.AIRTABLE_BASE_ID = '';
